10ed6e1bfbf6fe4f6a01b689766ef78eea42f804
Angela <angelalamluu@gmail.com>
2021-03-28 00:38:18 -0700

    created API request through seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#response will be a string
+response = RestClient.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+#we parse it into a hash
+pokemons = JSON.parse(response)["results"]#add number at end for pokemon
 
-binding.pry
+pokemons.each do |pokemon|
+    Pokemon.create(name: pokemon["name"])
+end
+
+#In rails, we take API data and turn it into ruby objects
