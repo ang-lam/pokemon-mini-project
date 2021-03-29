5ee60e35947cf66adbc9019143b097c7ad8d04a1
Angela <angelalamluu@gmail.com>
2021-03-29 16:34:09 -0700

    added trainer drop down menu for new team form

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
     belongs_to :trainer
     POKEMONS = Pokemon.all.collect { |pokemon| pokemon.name.capitalize }
+    TRAINERS = Trainer.all.collect { |trainer| trainer.name.split.map(&:capitalize).join(' ') }
 end
